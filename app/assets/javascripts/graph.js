aa55b7bf1fc146f433b08ef1be10a9c418d90926
K Mak <karen.mak@stx.oxon.org>
2014-03-27 17:06:16 -0400

    added y axis to graph

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -77,26 +77,37 @@ Gastropangs.fetchMealsByDOW = function() {
 };
 
 Gastropangs.drawGraph = function(meals) {
-  colorScale= d3.scale.category10();
-     var margin = {top: 20, right: 10, bottom: 20, left: 50},
+   var margin = {top: 20, right: 10, bottom: 20, left: 50},
          width = 350 - margin.left - margin.right,
-         height = 400 - margin.top - margin.bottom,
+         height = 600 - margin.top - margin.bottom,
          padding = 50;
 
-     var yScale = d3.scale.linear()
+   var colorScale= d3.scale.category10();
+
+   var yAxisScale = d3.scale.linear()
+                          .domain([d3.max(meals, function(m) { return m.level_of_fullness;}), 0])
+                          .range([padding, height]);
+   var yScale = d3.scale.linear()
                           .domain([0, d3.max(meals, function(m) { return m.level_of_fullness;})])
-                          .range([0, height]);
+                          .range([0, height-padding]);
 
-     var tip = d3.tip()
-                 .attr('class', 'd3-tip')
-                 .offset([-10, 0])
-                 .html(function(m) { return "Date: <span class='date'>" + m.date + "</span><br> Satiety: <span class='satiety'>" + m.level_of_fullness + "</span>";});
+   var tip = d3.tip()
+               .attr('class', 'd3-tip')
+               .offset([-10, 0])
+               .html(function(m) { return "Date: <span class='date'>" + m.date + "</span><br> Satiety: <span class='satiety'>" + m.level_of_fullness + "</span>";});
 
-     var graph = d3.select('#graph')
+   var graph = d3.select('#graph')
                   .attr('height', height)
                   .attr('width', width);
 
+   var yAxis = d3.svg.axis()
+                     .orient('left')
+                     .scale(yAxisScale)
+                     .ticks(5);
+
+
      graph.call(tip);
+
 
      graph.selectAll('rect')
           .data(meals)
@@ -105,14 +116,25 @@ Gastropangs.drawGraph = function(meals) {
             .attr('class', 'bar')
             .attr('fill', function(m) { return colorScale(m.time); })
             .attr('height', function(m) { return yScale(m.level_of_fullness); })
-            .attr('width', 15)
+            .attr('width', 20)
             .attr('x', function(m, i) { return i * 25; })
             .attr("y", function(m) { return (height - yScale(m.level_of_fullness)); })
             .on('mouseover', tip.show)
             .on('mouseout', tip.hide);
+
+    graph.append('g')
+         .attr('class', 'yaxis')
+         .attr('height', height)
+         .attr('x', 0)
+         .attr("transform", "translate("+ width +",0)")
+         .call(yAxis)
+         .selectAll('text')
+         .attr('dy', '1em')
+         .attr("transform", function(m) {return "rotate(40)";});
 };
 
 Gastropangs.drawLegend = function(meals){
+  var colorScale= d3.scale.category10();
 
   var legend = d3.select('#legend')
                   .attr('height', 100)
