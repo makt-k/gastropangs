07b7d9e519246e022b4afe9c05e80d71199cbd47
K Mak <karen.mak@stx.oxon.org>
2014-03-27 18:44:21 -0400

    created association and validation tests for meal model

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Meal do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:level_of_fullness) }
+    it { should validate_presence_of(:time) }
+    it { should validate_presence_of(:date) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:user) }
+  end
 end
